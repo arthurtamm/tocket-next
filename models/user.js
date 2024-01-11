ba2a8b53ad2ed529e224b89c3ef0de6f39e25754
CaioBoa <caioob@al.insper.edu.br>
2024-01-11 16:17:30 -0300

    Responsividade Header

--- models/user.js
+++ models/user.js
@@ -11,14 +11,14 @@ const UserSchema = new Schema({
         required: [true, 'Username is required.'],
         match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
-    phoneNumber: {
-        type: String,
-        required: [true, 'Phone number is required.'],
-        match: [
-            /^(?:\+55)?(?:\d{2})?(9\d{8})$/,
-            'Phone number invalid, it should be a valid Brazilian phone number!',
-        ],
-    },
+    // phoneNumber: {
+    //     type: String,
+    //     required: [true, 'Phone number is required.'],
+    //     match: [
+    //         /^(?:\+55)?(?:\d{2})?(9\d{8})$/,
+    //         'Phone number invalid, it should be a valid Brazilian phone number!',
+    //     ],
+    // },
     image: {
         type: String,
     }
