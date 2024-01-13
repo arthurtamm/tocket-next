ce4fabbe3f1bed679b7baf4df01f7226bc80cdee
gabrielmmh <gabrielmmh@al.insper.edu.br>
2024-01-13 11:44:41 -0400

    hover dos eventos aplicado

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,71 +1,58 @@
 "use client";
 
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { AiOutlineSearch } from "react-icons/ai";
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Loading from '@components/Loading';
 
-const EventList = ({ data }) => {
-
-    const [isShown, setIsShown] = useState(0);
+const EventItem = ({ event }) => {
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
-    <>
-      <div className='sm:flex hidden mt-10 h-full flex flex-row justify-between flex-wrap'>
-        {data.map((event) => (
-            <div className='h-full flex justify-center items-center mb-4 mr-5 ml-5'>
-                <Link onMouseEnter={() => setIsShown(event._id)} onMouseLeave={() => setIsShown(0)} href={`/events/${event._id}`}>
-                    {isShown === event._id ? (
-                    <div className='w-200 h-200'>
-                        <h1 className='text-white text-4xl'>
-                                {event.title}
+        <div
+            className='flex justify-center items-center mb-4 mr-5 ml-5'
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <Link href={`/events/${event._id}`}>
+                {isHovered ? (
+                    <div className='flex justify-center items-center max-h-[200px] min-h-[200px] max-w-[350px] min-w-[350px] rounded-xl'>
+                        <h1 className='text-white text-4xl text-center'>
+                            {event.title}
                         </h1>
                     </div>
-                    ) : (                    
-                    <Image className='max-h-[200px] min-h-[200px] max-w-[350px] min-w-[350px] rounded-xl'
+                ) : (
+                    <Image
+                        className='max-h-[200px] min-h-[200px] max-w-[350px] min-w-[350px] rounded-xl'
                         src={event.image}
                         alt={event.title}
-                        width={200}
-                        height={200}
-                    />) }
-                </Link>
-            </div>
-        ))}
-      </div>
-
-      <div className='sm:hidden mt-4 w-full flex flex-col'>
-        {data.map((event) => (
-            <div className='w-full flex justify-center mb-4'>
-                <Link className='w-3/4' href={`/events/${event._id}`}>
-                    <Image className='w-full max-h-[150px] min-h-[150px] rounded-xl'
-                        src={event.image}
-                        alt={event.title}
-                        width={200}
+                        width={350}
                         height={200}
                     />
-                </Link>
+                )}
+            </Link>
+        </div>
+    );
+};
+
+
+const EventList = ({ data }) => {
+    return (
+        <>
+            <div className='sm:flex hidden mt-10 h-full flex flex-row justify-between flex-wrap'>
+                {data.map(event => <EventItem key={event._id} event={event} />)}
             </div>
-        ))}
-      </div>
-    </>
-    )
-}
+            {/* O restante da renderização para dispositivos menores, caso necessário */}
+        </>
+    );
+};
 
 const Feed = () => {
-    // const response = await fetch('/api/event');
-    // const events = await response.json();
-    const [events, setEvents ] = useState([]);
+    const [events, setEvents] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [searchEvent, setSearchEvent] = useState({ title: '', image: '', date: new Date(), place: '', });
 
-    const [searchEvent, setSearchEvent] = useState({
-        title: '',
-        image: '',
-        date: new Date(),
-        place: '',
-      });
-  
     useEffect(() => {
         const fetchEvents = async () => {
             try {
@@ -77,9 +64,9 @@ const Feed = () => {
             } finally {
                 setLoading(false);
             }
-    };
+        };
 
-    fetchEvents();
+        fetchEvents();
     }, []);
 
     const handleSearchChange = (e) => {
@@ -88,60 +75,14 @@ const Feed = () => {
 
     const filteredEvents = events.filter(
         (event) => event.title.toLowerCase().includes(searchEvent.title.toLowerCase())
-    );  
+    );
 
     return (
-        <section className= 'flex w-full h-full flex-col items-center bg-indigo-950'>
-            
-            <section className= 'sm:flex hidden flex w-full mt-10 flex-row justify-center items-center'>
-                
-                <div className= 'flex flex-col justify-start'>
-                    <h1 className='text-white mr-20 text-4xl'>EVENTOS</h1>
-                    <h1 className='text-white mr-20 text-3xl'>BOMBANDO</h1>
-                </div>
-                
-                <form className='w-1/3'>
-                    <div className='relative'>
-                        <input 
-                            type="text"
-                            placeholder='Buscar eventos'
-                            className='w-full h-10 rounded-full pl-10'
-                            value={searchEvent.title}
-                            onChange={handleSearchChange}
-                        />
-                        <span className='absolute left-5 top-[50%]' style={{ transform: 'translate(-50%, -50%)'}}>
-                            <AiOutlineSearch className='text-gray-500' />
-                        </span>
-                    </div>
-                </form>
-            </section>
-
-            <section className= 'sm:hidden flex w-full h-full mt-2 flex-row justify-center items-center'>
-                
-                <div className= 'flex flex-col justify-start'>
-                    <h1 className='text-white ml-2 mr-5 text-2xl'>EVENTOS</h1>
-                    <h1 className='text-white ml-2 mr-5 text-xl'>BOMBANDO</h1>
-                </div>
-                
-                <form className='w-full mr-2'>
-                    <div className='relative'>
-                        <input 
-                            type="text"
-                            placeholder='Buscar eventos'
-                            className='w-full h-10 rounded-full pl-10'
-                            value={searchEvent.title}
-                            onChange={handleSearchChange}
-                        />
-                        <span className='absolute left-5 top-[50%]' style={{ transform: 'translate(-50%, -50%)'}}>
-                            <AiOutlineSearch className='text-gray-500' />
-                        </span>
-                    </div>
-                </form>
-            </section>
-
+        <section className='flex w-full h-full flex-col items-center bg-indigo-950'>
+            {/* Seções de pesquisa e cabeçalho, se necessário */}
             {loading ? <Loading /> : <EventList data={filteredEvents} />}
         </section>
-  )
-}
+    );
+};
 
-export default Feed
+export default Feed;
