e4146e4cb8f6e035746464fb4917c8fdd6ec2bd2
arthurtamm <arthurmtamm@gmail.com>
2024-02-07 19:05:36 -0300

    email provider auth

--- utils/database.js
+++ utils/database.js
@@ -12,9 +12,7 @@ export const connectToDB = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "tocket",
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            dbName: "tocket"
         })
 
         isConnected = true;
