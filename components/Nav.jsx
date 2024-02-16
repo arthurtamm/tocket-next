2149deee2ed582f888b1069d37c418d9e8cfe6db
arthurtamm <arthurmtamm@gmail.com>
2024-02-15 21:30:27 -0300

    consertando bugs

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,15 +2,15 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { AiOutlineHome, AiOutlineSwap, AiOutlineUser } from "react-icons/ai";
+import HamburgerMenu from './Burger';
 import { signIn, signOut, useSession, getProviders} from 'next-auth/react';
+import Dropdown from '@components/Dropdown';
 
 const Nav = () => {
     const { data: session } = useSession();
-
     const [providers, setProviders] = useState(null);
-
     const [navSticky, setNavSticky] = useState(false);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -34,10 +34,11 @@ const Nav = () => {
     
         setUpProviders();
       }, [])
+
     return (
         <nav className={`transition-all duration-300 w-full top-0 z-10 bg-indigo-900 ${navSticky ? 'sticky' : 'bg-transparent'}`}>
-            <div className='flex justify-around py-5'>
-                <Link href="/" className="flex items-center ml-10">
+            <div className='flex justify-between py-5 px-5 md:mx-10'>
+                <Link href="/" className="flex items-center">
                     <Image
                         src="/assets/images/logo.png"
                         alt="Logo"
@@ -45,93 +46,45 @@ const Nav = () => {
                         height={30}
                     />
                 </Link>
-
-                <Link href="/about" className="sm:flex hidden text-white mx-2  flex items-center">
-                    <AiOutlineUser className="mr-2" /> Sobre Nós
-                </Link>
-                <Link href="/project" className="sm:flex hidden text-white mx-2 flex items-center">
-                    <AiOutlineSwap className="mr-2" /> Marketplace
-                </Link>
                 
                 <div className='flex relative'>
-                {session?.user ? (
-                    <>
-                        <div className='flex'>
-                            <Image
-                                src={"/assets/images/icon-three-bars.jpg"}
-                                width={37}
-                                height={37}
-                                alt='profile'
-                                onClick={() => setToggleDropdown((prev) => !prev)}
-                            />
-
-                            {toggleDropdown && (
-                                <div className="dropdown bg-black z-50">
-                                    <Link
-                                        href='/profile'
-                                        className="dropdown_link"
-                                        onClick={() => setToggleDropdown(false)}
-                                    >
-                                        Perfil
-                                    </Link>
-
-                        
-                                    <Link
-                                        href='/sobre'
-                                        className="sm:hidden dropdown_link"
-                                        onClick={() => setToggleDropdown(false)}
-                                    >
-                                        Sobre nós
-                                    </Link>
-                                    <Link
-                                        href='/project'
-                                        className=" sm:hidden dropdown_link" 
-                                        onClick={() => setToggleDropdown(false)}
-                                    >
-                                        Marketplace
-                                    </Link>
-                                    <button
-                                        type="button"
-                                        onClick={() => {
-                                            setToggleDropdown(false);
-                                            signOut();
-                                        }}
-                                        className='text-white'
-                                    >
-                                        Sair
-                                    </button>
+                    {session?.user ? (
+                        <>
+                                <div className='flex'>
+                                    <HamburgerMenu />
+                                    <Dropdown
+                                        toggleDropdown={toggleDropdown}
+                                        setToggleDropdown={setToggleDropdown}
+                                    />
                                 </div>
-                            )}
-                        </div>
-                    </>
-                ): (
-                    <>
-                        {providers && 
-                            Object.values(providers).map((provider) => (
+                            </>
+                    ): (
+                        <>
+                            {providers && 
+                                Object.values(providers).map((provider) => (
 
-                                // <button
-                                //     type="button"
-                                //     key={provider.name}
-                                //     onClick={() => signIn(provider.id)}
-                                //     href='/login'
-                                //     className='btn text-white p-2'
-                                // >
-                                //     Entrar
-                                // </button>
-                                <Link
-                                    href='/login'
-                                    className='bg-white'
-                                >
-                                    Entrar
-                                </Link>
-                            ))
-                        }
-                    </>
-                )}
+                                    // <button
+                                    //     type="button"
+                                    //     key={provider.name}
+                                    //     onClick={() => signIn(provider.id)}
+                                    //     className='btn text-white p-2'
+                                    // >
+                                    //     Entrar
+                                    // </button>
+                                    <Link
+                                        href='/api/auth/signin'
+                                        className='bg-white'
+                                    >
+                                        Entrar
+                                    </Link>
+                                ))
+                            }
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Nav;
