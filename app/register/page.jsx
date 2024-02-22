497d8faa94bb46326d931c935ac6333392c38d5b
gabrielmmh <gabrielmmh@al.insper.edu.br>
2024-02-22 15:52:31 -0300

    corrigindo bug de import

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,7 +2,6 @@ import RegisterForm from "@components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@app/api/auth/[...nextauth]/route";
-import RegisterFormExample from "@components/RegisterFormExample";
 
 const Register = async () => {
   const session = await getServerSession(authOptions);
